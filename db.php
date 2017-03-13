c83867e8017fc22924652308faebad5d6cfb4bed
leo-lizhuoxuan <437792560@qq.com>
2017-03-13 13:32:23 +0800

    add db.php
    add db.php

---
+++ db.php
@@ -0,0 +1,17 @@
+<!-- This page used to connect the database -->
+<?php
+$username = "root";
+$password = "";
+$hostname = "localhost";
+//connection to the database
+$db = mysql_connect ($hostname, $username, $password)
+     or die ("Unable to connect to MySQL");
+	 echo "  ";
+?>
+	 
+<?php
+//selectselect a database to work with
+$selected = mysql_select_db("carwarshing",$db)
+           or  die("Could not select database");
+?>
+
