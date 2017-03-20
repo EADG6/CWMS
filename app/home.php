13e5cdaa16321a14b169880a9e99317b62b24b86
qq398935137 <398935137@qq.com>
2017-03-20 22:53:02 +0800

    add pictures in home page

--- app/home.php
+++ app/home.php
@@ -37,17 +37,17 @@
 
         <div class="row">
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <img class="img-circle img-responsive img-center" src="../CWMS/static/img/3.jpg" alt="">
                 <h2>Marketing Box #1</h2>
                 <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
             </div>
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <img class="img-circle img-responsive img-center" src="../CWMS/static/img/7.jpg" alt="">
                 <h2>Marketing Box #2</h2>
                 <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
             </div>
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <img class="img-circle img-responsive img-center" src="../CWMS/static/img/6.jpg" alt="" >
                 <h2>Marketing Box #3</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
             </div>
