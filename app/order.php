75cf19bf583562c2b3acc521f914e1f89d5154c0
leo-lizhuoxuan <437792560@qq.com>
2017-03-21 19:46:56 +0800

    delete
    delete unused words

--- app/order.php
+++ app/order.php
@@ -18,9 +18,6 @@
                 <h1>ORDERS</h1>
 				<hr>
 			</div>
-				<div class="col-sm-12 col-md-offset-0">
-					There are 5 cars befour you.
-				</div class="col-sm-12 col-md-offset-1">
 				<h1><a href="index.php?page=addorder">Add New Order</a></h1>
 				
 				<table class="zebra">
