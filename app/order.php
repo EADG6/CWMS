9ec2fe67db00afac0112f4415fbb5c68a3a7d770
MarshallCN <collapselzh@live.com>
2017-04-14 23:47:52 +0800

    update order rate page in front-ed

--- app/order.php
+++ app/order.php
@@ -1,4 +1,28 @@
-   <?php
+<script>
+	/* Change order rate */
+	function rateStar(num,orid){
+		for(var i=0;i<(num+1);i++){
+			$($('#stars'+orid).children()[i]).css('color','yellow')
+		}
+	}
+ 	function hideStar(num,orid,orig){
+		for(var i=5;i>(orig-1);i--){
+			$($('#stars'+orid).children()[i]).css('color','#fff')
+		}
+	}
+	function changeRate(starnum,orid){
+		starnum++;
+		$.ajax({
+			url:'admin/ajax.php',
+			data:{"rate":starnum,"orderid":orid},
+			type:'POST',
+			success:function(data){
+				window.location.href='index.php?page=order'
+			}
+		});
+	}
+</script>
+<?php
    if (isset($_GET['id'])){
    	$id =$_GET['id'];
    	//delete the menu
@@ -10,9 +34,8 @@
 		session_destroy();
 		echo "<script>alert('Please Login First');location.href='login.php'</script>";
 	}
-    ?>
+?>
 <div class="container">
-	<h1>ORDERS</h1>
 		<h1><a href="index.php?page=addorder" class="addorder">Add New Order</a></h1>
 			<div class="col-sm-12" style="padding:20px;background:#fff;border-radius:5px;">
 				<table class="table table table-striped">
@@ -22,24 +45,30 @@
 							<th>Plate</th>
 							<th>Time</th>
 							<th>Rate</th>
-							<th>Comment</th>
 							<th>Status</th>
 							<th>Delete</th>
 						<tr>
 					</thead>
-						<?php
+				<?php
 					$sql_orders = "SELECT o.id,c.plate,CONCAT(Date,' ',Time) AS time,o.status,o.rate FROM orders AS o INNER JOIN order_service AS os ON o.id=os.order_id INNER JOIN car AS c ON c.id=os.car_id WHERE o.cus_id='".$_SESSION['customer_id']."'"; 
 					$result_orders = $mysql->query($sql_orders);
 					while ($row_orders = $mysql->fetch($result_orders)){
-						?>							       
+				?>							       
 		            <tr>
 					 <td><?php echo $row_orders['id']; ?></td>
 		             <td><?php echo $row_orders['plate']; ?></td>
 		             <td><?php echo $row_orders['time']; ?></td>
-		             <td><?php echo $row_orders['rate']; ?></td>
-					 <td>
-						<button type="button" class="btn btn-primary">Rate</button>
-					 </td>
+		             <?php //echo $row_orders['rate']; ?>
+		             <td>
+						<button type="button" class="btn btn-primary" id='stars<?php echo $row_orders['id']; ?>'>
+					<?php
+						for($i=0;$i<5;$i++){
+							$star = $row_orders['rate']>$i?'style="color:yellow"':'' ;
+							echo "<i class='fa fa-star stars' $star onclick='changeRate($i,".$row_orders['id'].")' onmouseover='rateStar($i,".$row_orders['id'].")' onmouseout='hideStar($i,".$row_orders['id'].",".$row_orders['rate'].")'></i>";
+						}
+					?>
+						</button>
+					</td>
 					 <td>
 					<?php 
 						switch($row_orders['status']){
