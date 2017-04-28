2ba02c8ae6cb290a1a7225e897f16b120e891e05
MarshallCN <collapselzh@live.com>
2017-04-28 16:47:39 +0800

    fixed bug in add order page

--- app/addorder.php
+++ app/addorder.php
@@ -33,15 +33,7 @@
 	}
 	?>
 	<div class="col-sm-8 col-sm-offset-2">
-		<?php
-		$sql_orders = "SELECT c.*,CONCAT(username,'-',FirstName,' ',LastName) as user FROM car AS c INNER JOIN customer AS cu ON c.cus_id=cu.id WHERE cus_id ='".$_SESSION['customer_id']."'";
-		$result_orders = $mysql->query($sql_orders);
-		$row_orders = $mysql->fetch($result_orders);
-		?>
 		<form method="post"> 
-			<div class="form-group">
-				<label>Current User: <?php echo $row_orders['user'];?></label>
-			</div>
 			<div class='form-group'>
 				<label for='emp'>Worker: <span id='empName'></span></label>
 				<input name='emp_id' id='emp' class='form-control selectid' onchange="selectCus(this,'empQueryRes','empName')" list='empQueryRes' placeholder="ID/Username" onfocus="this.autocomplete='off'" onblur="this.autocomplete='on'">
@@ -59,10 +51,12 @@
 				<label>Choose Car:</label>
 				<select name="id" class="form-control"> 
 				<?php
-					while ($row_orders = $mysql->fetch($result_orders)){
+					$sql_car = "SELECT c.*,CONCAT(username,'-',FirstName,' ',LastName) as user FROM car AS c INNER JOIN customer AS cu ON c.cus_id=cu.id WHERE cus_id ='".$_SESSION['customer_id']."'";
+					$result_car = $mysql->query($sql_car);
+					while ($row_car = $mysql->fetch($result_car)){
 				?>
-					<option value="<?php echo $row_orders['id']; ?>">
-						<?php echo $row_orders['plate']; ?>
+					<option value="<?php echo $row_car['id']; ?>">
+						<?php echo $row_car['plate']; ?>
 					</option> 
 				<?php
 					}
@@ -72,13 +66,13 @@
 			<div class="form-group">
 				<label>Choose Services:</label>
 				<?php
-					$sql_serviers = "SELECT * FROM product_service WHERE type_id = 1 AND Price IS NOT NULL";
-					$result_serviers = $mysql->query($sql_serviers);
-					while ($row_serviers = $mysql->fetch($result_serviers)){
+					$sql_service = "SELECT * FROM product_service WHERE type_id = 1 AND Price IS NOT NULL";
+					$result_service = $mysql->query($sql_service);
+					while ($row_service = $mysql->fetch($result_service)){
 				?>
 				<div class="form-group col-sm-offset-1">
-					<label><?php echo $row_serviers['product_name']; ?>:</label>
-					<input type="checkbox" class='form-control' name="services[]" value="<?php echo $row_serviers['id']; ?>">
+					<label><?php echo $row_service['product_name']; ?>:</label>
+					<input type="checkbox" class='form-control' name="services[]" value="<?php echo $row_service['id']; ?>">
 				</div>
 				<?php
 					}
