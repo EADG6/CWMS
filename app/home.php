6a0dc9ec7b8af3e99d41a177a99123e2ee0b814b
leo-lizhuoxuan <437792560@qq.com>
2017-05-18 19:43:59 +0800

    update information in home pang

--- app/home.php
+++ app/home.php
@@ -6,21 +6,21 @@
         <div class="row">
             <div class="col-sm-8" style="border-radius:5px; background:white; padding-bottom:40px">
                 <h2>What We Do</h2>
-                <p>Introduce the visitor to the business using clear, informative text. Use well-targeted keywords within your sentences to make sure search engines can find the business.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et molestiae similique eligendi reiciendis sunt distinctio odit? Quia, neque, ipsa, adipisci quisquam ullam deserunt accusantium illo iste exercitationem nemo voluptates asperiores.</p>             
+                <p>Fresh Car Washing shop is focus on dedicated to solving auto beauty, car maintenance and maintenance.</p>
+                <p>For now in CDUT DongYuan region we have collected more than 300 members. We will take good care of your car; make your car keep optimum state.</p>             
             </div>
             <div class="col-sm-3" style="border-radius:5px; background:white; padding-bottom:20px; margin-left:30px">
                 <h2>Contact Us</h2>
                 <address>
-                    <strong>Start Bootstrap</strong>
+                    <strong>Fresh Car Washing Shop</strong>
                     <br>3481 Melrose Place
-                    <br>Beverly Hills, CA 90210
+                    <br>Chengdu city Chenghua District road Erxian Qiaodong 
                     <br>
                 </address>
                 <address>
-                    <abbr title="Phone">P:</abbr>(123) 456-7890
+                    <abbr title="Phone">P:</abbr>(+86) 155-2815-3691
                     <br>
-                    <abbr title="Email">E:</abbr> <a href="mailto:#">name@example.com</a>
+                    <abbr title="Email">E:</abbr> <a href="mailto:#">Tiankaiyuan@bibijz.com </a>
                 </address>
             </div>
         </div>
@@ -31,18 +31,20 @@
         <div class="row">
             <div class="col-sm-4">
                 <img class="img-circle img-responsive img-center" src="../CWMS/static/img/3.jpg" alt="">
-                <h2>Marketing Box #1</h2>
-                <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
+                <h2>Ordinary member</h2>
+                <p>Recharge 400 RMB per car; SUV and MPV recharge 500 RMB Member wash car 20 RMB for small car, SUV and MPV cost 25 every time; Give a free car wax for one car; Give a free Windshield washer fluid for one year; For each ordinary member upper limit three cars.                   </p>
             </div>
             <div class="col-sm-4">
                 <img class="img-circle img-responsive img-center" src="../CWMS/static/img/7.jpg" alt="">
-                <h2>Marketing Box #2</h2>
-                <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <h2>Silver member</h2>
+                <p>Recharge 800 RMB per car; SUV and MPV recharge 1000 RMB Member wash car 15 RMB for small car, SUV and MPV cost 20 every time; Give a free car wax for two cars; Give a free windshield washer fluid for one year; Routine inspection of car for three times; Cars inspection one time for free For each silver member upper limit three cars.
+                </p>
             </div>
             <div class="col-sm-4">
                 <img class="img-circle img-responsive img-center" src="../CWMS/static/img/6.jpg" alt="" >
-                <h2>Marketing Box #3</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <h2>Gold Member</h2>
+                <p>Recharge 1500 RMB per car; SUV and MPV recharge 2000 RMB Member wash car 10 RMB for small car, SUV and MPV cost 15 every time; Give a free car wax for four cars; Give a free car polishing for twice; Give a free windshield washer fluid for one year; Routine inspection of car for three times; Cars inspection one time for free For each silver member upper limit five cars.
+                </p>
             </div>
         </div>
     </div>
